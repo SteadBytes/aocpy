490d7e922f0a55f300b09037924a3007118dc480
Ben Steadman <steadmanben1@gmail.com>
2018-12-02 19:00:20 +0000

    include part_1/part_2 methods in solution template

--- aocpy/templates/solution.py
+++ aocpy/templates/solution.py
@@ -1,8 +1,17 @@
-def main(puzzle_input):
+def part_1():
     pass
+
+
+def part_2():
+    pass
+
+
+def main(puzzle_input_f):
+    lines = puzzle_input_f.readlines()
+    print("Part 1: ", part_1())
+    print("Part 2: ", part_2())
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
-        puzzle_input = f.read()
-    print(main(puzzle_input))
+        main(f)
