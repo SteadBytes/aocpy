e66efed9e708739412650e3b7214f3bcef9249c4
Ben Steadman <steadmanben1@gmail.com>
2018-12-25 12:16:36 +0000

    rollback version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ URL = "https://github.com/SteadBytes/aocpy"
 EMAIL = "steadmanben1@gmail.com"
 AUTHOR = "Ben Steadman"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.4"
+VERSION = "0.0.1"
 
 REQUIRED = ["requests", "beautifulsoup4", "click", "pytz"]
 
