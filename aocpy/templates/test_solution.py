9e72bfd696f147314a905122c3d8fba77b8930b0
Lennon Cook <lvc@codetripping.net>
2020-11-30 23:50:27 +1100

    Include some basic pytest boilerplate in the daily template

---
+++ aocpy/templates/test_solution.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import pytest
+import solution
+
+@pytest.mark.parametrize('data,expect',
+    [('sample_input_1', 'sample_output_1'),
+     ('sample_input_2', 'sample_output_2')]
+)
+def test_part1(data, expect):
+    assert solution.part_1(data) == expect
+
+@pytest.mark.parametrize('data,expect', 
+    [('sample_input_1', 'sample_output_1'),
+     ('sample_input_2', 'sample_output_2')]
+)
+def test_part2(data, expect)
+    assert solution.part_2(data) == expect
+
